6f165ade68638a36efeaee31af2e3a465b71afc8
AreeshRazaKhan <razaareesh2651@gmail.com>
2023-10-03 13:43:54 +0500

    final

--- assests/js/index.js
+++ assests/js/index.js
@@ -1,8 +1,4 @@
 var code = document.getElementById('compiler');
-// function htmlCompiler(text) {
-//     var displayCanvas = document.getElementById('iframe').contentDocument.body;
-//     displayCanvas.innerHTML = code.value;
-// }
 
 function quickAdd(tagName) {
     var inputText = document.getElementById(tagName + '-text'); //h1-text or p-text
@@ -16,10 +12,54 @@ function quickAdd(tagName) {
         code.value += `\n<${tagName}>${inputText.value}</${tagName}>`;
     }
     runCode()
-    inputText.value = src.value = width.value = height.value = "";
+    // inputText.value = src.value = width.value = height.value = "";
 }
 
 function runCode() {
     var displayCanvas = document.getElementById('iframe').contentDocument.body;
     displayCanvas.innerHTML = code.value;
 }
+
+var buttonClickedAtt;
+function dragStart(e) {
+    buttonClickedAtt = e.querySelector('button')
+}
+
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+function onDrop(event) {
+    event.preventDefault();
+    buttonClickedAtt.click();
+    code.style.backgroundColor = "transparent"
+}
+
+code.addEventListener('dragenter', (e) => {
+    code.style.backgroundColor = "aliceblue"
+})
+
+code.addEventListener('dragleave', (e) => {
+    code.style.backgroundColor = "transparent"
+})
+
+
+// add shopify draggable plugin in buttons
+// const sortable = new Draggable.Sortable(document.querySelectorAll('.quickadd'), {
+//     draggable: '.drag-item',
+//     handle: '.drag-handler',
+//     classes: {
+//         'draggable:over': ['draggable--over', 'opacity-25'],
+//         'source:dragging': ['draggable-source--is-dragging', 'opacity-0'],
+//     },
+//     mirror: {
+//         constrainDimensions: true,
+//         xAxis: false,
+//     },
+//     swapAnimation: {
+//         duration: 200,
+//         easingFunction: 'ease-in-out',
+//         horizontal: false,
+//     },
+//     plugins: [Draggable.Plugins.ResizeMirror, Draggable.Plugins.SwapAnimation],
+// });
